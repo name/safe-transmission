cdfd7ebb38e0565b986a5ee4b1d7449ee0943b3c
Charlie <charlie@maddex.co>
2022-05-03 21:33:44 +0100

    added demo

---
+++ Demo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Specialized;
+
+using SafeTransmission;
+
+namespace Demo {
+    class Program {
+        static void Main(string[] args) {
+
+            /* Setup SafeTransmission */
+            SafeTransmission.SafeTransmission safeTransmission = new SafeTransmission.SafeTransmission("EncryptionKey");
+            safeTransmission.NullifyProxy = true;
+            NameValueCollection test = new NameValueCollection();
+
+            /* Set POST Data & Request */
+            test["key"] = "value";
+            Response response = safeTransmission.Request("https://maddex.co/demo.php", test);
+
+            /* Display Reponse Data */
+            Console.WriteLine($"Raw: {response.raw}");
+            Console.WriteLine($"Status: {response.status}");
+            Console.WriteLine($"Message: {response.message}");
+            Console.WriteLine($"Extra: {response.GetData<string>("charlie")}");
+            Console.WriteLine($"POST Data: {response.GetData<string>("key")}");
+
+            Console.ReadLine();
+        }
+    }
+}
